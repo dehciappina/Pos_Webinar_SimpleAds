640fbfaea322c967146e60b03904c1dbf0fe2bd2
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-07-07 15:37:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fixedCtnr = document.querySelector('#fixed_ctnr')
 
 document.body.onscroll = function() {
-    if(window.pageYOffset > (window.innerHeight / 6)) {
+    if(window.pageYOffset > 50) {
         fixedCtnr.style.transform = "translate(0)"
     } else {
         fixedCtnr.style.transform = "translateY(100%)"
